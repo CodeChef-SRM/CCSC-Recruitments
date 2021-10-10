4cfe04af3bc7fb5785d755b6eff5790d39d8e5dd
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-10 15:12:32 +0530

    fix: fixed build files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "CCSC Recruitments: Django Server",
-      script: "chmod +x ./run.sh && ./run.sh",
+      script: "chmod +x ./run.sh && . ./run.sh",
     }
   ]
 };
