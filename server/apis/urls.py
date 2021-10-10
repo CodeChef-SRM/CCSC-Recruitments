17f9a42a12c915b63796be5c32673f38ccba3f06
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-11 01:49:09 +0530

    feat: Added reset-password route

--- server/apis/urls.py
+++ server/apis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import Login, Register, ForgotPassword
+from authentication.views import Login, Register, ForgotPassword, ResetPassword
 
 
 urlpatterns = [
     path("login", Login.as_view()),
     path("register", Register.as_view()),
     path("forgot-password", ForgotPassword.as_view()),
+    path("reset-password", ResetPassword.as_view()),
 ]
