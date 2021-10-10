3dd7e78ae00c202a699b1a8e4659d68c5dd7bf2f
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-10 18:52:25 +0530

    🔨 Updated scripts

---
+++ server_tests.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+error="\e[1;31m[ERROR]\e[0m"
+execution="\e[0;36m[INFO]\e[0m"
+
+
+echo "$execution [SERVER-BOOT]"
+. ./run.sh
+
+sleep 4s
+
+echo "$execution [STARTING SERVER TESTS]"
+
+var=$(python3 test_runner.py | grep "Failing" -c )
+
+if [ $var -eq 0 ];
+then
+    echo "$excution Passing Tests!"
+else
+    echo "$error Failing Tests!"
+    exit -1
+fi
