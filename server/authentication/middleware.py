e9bdcda17b5f7f3d1e5c36c70ef3b401e621ca9a
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-12 06:18:17 +0530

    feat: Added me verification route

--- server/authentication/middleware.py
+++ server/authentication/middleware.py
@@ -7,7 +7,7 @@ from .utils import check_token
 class AuthMiddleWare:
     def __init__(self, view):
         self.view = view
-        self._protected = ["/apis/reset-password"]
+        self._protected = ["/apis/reset-password", "/me"]
 
     @staticmethod
     def _validate_tokentype(authorization: str):
