5bc668d1d0f919ee2fc4db56f1483d36bf05e125
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-12 06:19:13 +0530

    feat: Added tests for me verification

--- server/tests/test_authentication.py
+++ server/tests/test_authentication.py
@@ -11,6 +11,9 @@ class TestAuthentication(unittest.TestCase):
         cls.headers = {"Content-Type": "application/json"}
         cls.base_url = "http://localhost:8000"
 
+    def setUp(self) -> None:
+        clear_all()
+
     def tearDown(self) -> None:
         clear_all()
 
@@ -50,3 +53,28 @@ class TestAuthentication(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("access_token", response.json())
         self.assertIn("refresh_token", response.json())
+
+    def test_me_verification(self):
+        user = get_user()
+        status = self.client.get(self.base_url + "/me")
+        self.assertEqual(status.status_code, 403)
+
+        response = self.client.post(
+            self.base_url + "/apis/register",
+            headers=self.headers,
+            data=json.dumps(user),
+        )
+        self.assertEqual(response.status_code, 201)
+        login_data = {"email": user["email"], "password": user["password"]}
+        response = self.client.post(
+            self.base_url + "/apis/login",
+            headers=self.headers,
+            data=json.dumps(login_data),
+        )
+        access_token = response.json()["access_token"]
+        headers = self.headers.copy()
+        headers.update({"Authorization": f"Bearer {access_token}"})
+        status = self.client.get(self.base_url + "/me", headers=headers)
+        self.assertEqual(status.status_code, 200)
+        self.assertIn("user", status.json())
+        self.assertIn("email", status.json())
